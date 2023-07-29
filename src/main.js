984feefbe0c7fbaa129017196c312226f82c006c
oskarrough <oskar@rough.dk>
2023-07-29 14:27:59 +0200

    Refactor imports

---
+++ src/main.js
@@ -0,0 +1,11 @@
+// import './local.js'
+import './spawn-worker.js'
+import {recordVisit} from './record-visit.js'
+
+// UI 
+import './r4-channels.js'
+import './r4-tracks.js'
+import './visit-counter.js'
+
+recordVisit()
+
--- src/record-visit.js
+++ src/record-visit.js
@@ -1,18 +1,18 @@
 import {getDb} from './local.js'
 
-async function recordVisit() {
+export async function recordVisit() {
   const db = await getDb()
   const id = 'visits'
+  // Get or create a count.
   const rows = await db.execO(`select count from counters where id = ?`, [id])
   let count = rows[0]?.count
   if (!count) {
     await db.exec(`insert into counters (id, count) values (?, ?)`, [id, 0])
     count = 0
   }
+  // Update count.
   count = count + 1
   await db.exec(`update counters set count = ? where id = ?`, [count, id])
   return count
 }
-
-recordVisit()
 
