a24ce559db42ad142e8948a3bffd0b4ee9104d0e
oskarrough <oskar@rough.dk>
2023-08-06 18:40:50 +0200

    Add comlink dependency

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,30 @@
 import atomico from '@atomico/vite'
+import {comlink} from 'vite-plugin-comlink'
 
 /** @type {import('vite').UserConfig} */
 export default {
-	plugins: [atomico()],
+	plugins: [comlink(), atomico()],
 	appType: 'mpa',
 	server: {
 		headers: {
 			'Cross-Origin-Opener-Policy': 'same-origin',
-			'Cross-Origin-Embedder-Policy': 'require-corp'
-		}
+			'Cross-Origin-Embedder-Policy': 'require-corp',
+		},
 	},
 	optimizeDeps: {
-		exclude: ['@sqlite.org/sqlite-wasm']
+		exclude: ['@sqlite.org/sqlite-wasm'],
 	},
 
 	build: {
 		target: 'esnext',
 	},
 	worker: {
+		plugins: [comlink()],
 		format: 'es',
 		rollupOptions: {
 			output: {
 				format: 'es',
 			},
-		}
+		},
 	},
 }
